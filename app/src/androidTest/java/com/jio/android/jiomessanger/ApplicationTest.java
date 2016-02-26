4538df715357afc93c2433b86e46b5a047d1adf5
Jiovanni gudino <TipiTorpedo@icloud.com>
2016-02-26 11:57:09 -0800

    first upload

---
+++ app/src/androidTest/java/com/jio/android/jiomessanger/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.hfad.messenger;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+public class CreateMessageActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+    //Call onSendMessage() when the button is clicked
+    public void onSendMessage(View view) {
+    }
+}
